19788f97b1f36d58f1574403e05a5880af3fa5e9
Josué Torres <josuejesusts@gmail.com>
2025-09-28 14:19:32 -0500

    feat: enhance gallery image overlay with improved hover effects and visibility

--- packages/frontend/src/components/layouts/Gallery/index.tsx
+++ packages/frontend/src/components/layouts/Gallery/index.tsx
@@ -3,18 +3,23 @@ export default function Gallery() {
     <div className="flex justify-center w-full">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4  max-w-7xl">
         <div className="grid gap-4">
-          <div className="relative group hover:scale-104 transition-transform duration-300 cursor-pointer easy-in-out ">
+          <div className="relative group hover:scale-104 transition-transform duration-300 cursor-pointer easy-in-out span-c-hover:opacity-">
             <img
               className="h-auto max-w-full rounded-lg"
               src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
               alt=""
             />
-            <div className="absolute inset-0 bg-gray-600 opacity-0 flex flex-col p-5 items-start justify-end rounded-lg transition-opacity duration-300 hover:opacity-25">
-              <div className="flex flex-col">
-                <span className="text-white text-lg font-semibold">
+            <div className="absolute inset-0 flex flex-col p-5 items-start justify-end rounded-lg">
+              {/* Fondo semitransparente */}
+              <div className="absolute inset-0 bg-gray-600 opacity-0 group-hover:opacity-25 rounded-lg transition-opacity duration-300 pointer-events-none"></div>
+              {/* Texto visible al hacer hover */}
+              <div className="relative z-10 flex flex-col">
+                <span className="opacity-0 group-hover:opacity-100 text-white text-lg font-semibold transition-opacity duration-300">
                   Product
                 </span>
-                <span className="text-white text-lg font-semibold">Price</span>
+                <span className="opacity-0 group-hover:opacity-100 text-white text-lg font-semibold transition-opacity duration-300">
+                  Price
+                </span>
               </div>
             </div>
           </div>

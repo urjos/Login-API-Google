e6fcba0ed201fb577bc00639cf151eb71a6da374
Josué Torres <josuejesusts@gmail.com>
2025-09-28 14:08:32 -0500

    feat: add overlay with product details and price to gallery images

--- packages/frontend/src/components/layouts/Gallery/index.tsx
+++ packages/frontend/src/components/layouts/Gallery/index.tsx
@@ -3,14 +3,22 @@ export default function Gallery() {
     <div className="flex justify-center w-full">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4  max-w-7xl">
         <div className="grid gap-4">
-          <div>
+          <div className="relative group hover:scale-104 transition-transform duration-300 cursor-pointer easy-in-out ">
             <img
               className="h-auto max-w-full rounded-lg"
               src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
               alt=""
             />
+            <div className="absolute inset-0 bg-gray-600 opacity-0 flex flex-col p-5 items-start justify-end rounded-lg transition-opacity duration-300 hover:opacity-25">
+              <div className="flex flex-col">
+                <span className="text-white text-lg font-semibold">
+                  Product
+                </span>
+                <span className="text-white text-lg font-semibold">Price</span>
+              </div>
+            </div>
           </div>
-          <div>
+          <div className="relative group hover:scale-104  transition-transform duration-300 cursor-pointer easy-in-out">
             <img
               className="h-auto max-w-full rounded-lg"
               src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"

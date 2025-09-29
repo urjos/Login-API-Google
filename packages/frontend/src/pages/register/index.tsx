4c2ed5056b974f89ce8d0ee755a0ba5c199af0a6
Josué Torres <josuejesusts@gmail.com>
2025-09-29 12:01:19 -0500

    feat(auth): crear formulario de registro de usuario
    Se añade el componente `RegisterForm` para la página de registro.
    
    - Se crea la interfaz de usuario con campos para nombre, apellido, email, país y contraseña.
    - El diseño es responsivo utilizando Tailwind CSS.
    - Este componente se renderiza en la ruta `/register` para permitir que nuevos usuarios se creen una cuenta.

--- packages/frontend/src/pages/register/index.tsx
+++ packages/frontend/src/pages/register/index.tsx
@@ -112,6 +112,7 @@ export const RegisterForm = () => {
                 type="password"
                 required
                 autoComplete="current-password"
+                placeholder="•••••••••"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 hover:shadow-sm transition easy-in-out duration-200"
               />
             </div>
@@ -120,20 +121,21 @@ export const RegisterForm = () => {
           <div>
             <div className="flex items-center justify-between">
               <label
-                htmlFor="password"
+                htmlFor="confirm_password"
                 className="block text-sm/6 font-medium text-gray-900"
               >
-                Password
+                Confirm Password
               </label>
               <div className="text-sm"></div>
             </div>
             <div className="mt-2">
               <input
-                id="password"
-                name="password"
+                id="confirm_password"
+                name="confirm_password"
                 type="password"
                 required
                 autoComplete="current-password"
+                placeholder="•••••••••"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 hover:shadow-sm transition easy-in-out duration-200"
               />
             </div>

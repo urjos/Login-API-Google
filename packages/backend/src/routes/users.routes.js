bfd3d78a2d43a2a44a816bc34c8f0c51829d4b98
Josué Torres <josuejesusts@gmail.com>
2025-09-29 13:30:31 -0500

    feat: Se agregaron las rutas para usuario

--- packages/backend/src/routes/users.routes.js
+++ packages/backend/src/routes/users.routes.js
@@ -6,4 +6,21 @@ router.get("/users", (req, res) => {
   res.send("en users");
 });
 
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send("en user " + id);
+});
+
+router.post("/users", (req, res) => {
+  res.send("creando un usuario");
+});
+
+router.delete("/users/:id", (req, res) => {
+  res.send("eliminando user");
+});
+
+router.put("/users/:id", (req, res) => {
+  res.send("actualizando user");
+});
+
 export default router;

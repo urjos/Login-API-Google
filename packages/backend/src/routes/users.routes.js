4fe16c92cda9b32c9612d46f148f9b6b19434ead
Josué Torres <josuejesusts@gmail.com>
2025-09-30 14:29:58 -0500

    feat: Conexión a la DB a través de endpoints

--- packages/backend/src/routes/users.routes.js
+++ packages/backend/src/routes/users.routes.js
@@ -5,36 +5,73 @@ const router = Router();
 
 router.get("/users", async (req, res) => {
   const [rows] = await pool.query("SELECT * FROM users");
-  res.json(rows);
+  if (rows.length === 0) {
+    return res.status(404).json({ message: "No users found" });
+  }
+  return res.json(rows);
 });
 
 router.get("/users/:id", async (req, res) => {
   const { id } = req.params;
-  const [rows] = await pool.query(`SELECT * FROM users WHERE id = ${id}`);
+  const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
 
   if (rows.length === 0) {
     return res.status(404).json({ message: "User not found" });
   }
-  res.json(rows);
+  return res.json(rows[0]);
 });
 
-router.post("/users", (req, res) => {
-  const data = req.body;
-  res.json(data);
+router.post("/users", async (req, res) => {
+  try {
+    const { name, email, password, country } = req.body;
+
+    const [result] = await pool.query(
+      "INSERT INTO users (name, email, password, country) VALUES (?, ?, ?, ?)",
+      [name, email, password, country]
+    );
+
+    return res.json({
+      id: result.insertId,
+      name,
+      email,
+      password,
+      country,
+      created_at: new Date(),
+      message: "User created successfully",
+    });
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      return res.status(409).json({ message: "El email ya está registrado." });
+    }
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
 });
 
 router.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
   const [result] = await pool.query("DELETE FROM users WHERE id = ?", [id]);
 
-  if (result.affectedRows === 0) {
+  if (result[0].affectedRows === 0) {
     return res.status(404).json({ message: "User not found" });
+  } else {
+    return res.sendStatus(204).json({ message: "User deleted successfully" });
   }
-  res.sendStatus(204);
 });
 
-router.put("/users/:id", (req, res) => {
-  res.send("actualizando user");
+router.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  const result = await pool.query("UPDATE users SET ? WHERE id = ?", [
+    data,
+    id,
+  ]);
+
+  if (result[0].affectedRows === 0) {
+    return res.status(404).json({ message: "Not updated" });
+  } else {
+    return res.sendStatus(204).json({ message: "User updated successfully" });
+  }
 });
 
 export default router;

7c4dc7bc841a158ad8e4921d6b5666a6bb117447
Josué Torres <josuejesusts@gmail.com>
2025-10-09 23:30:13 -0500

    feat: ajuste de margen

--- packages/frontend/src/components/layouts/Gallery/index.tsx
+++ packages/frontend/src/components/layouts/Gallery/index.tsx
@@ -3,7 +3,7 @@ import data from "../../../data.json";
 export default function Gallery() {
   return (
     <>
-      <div className="flex justify-center w-full mt-10">
+      <div className="flex justify-center w-full mt-5">
         <div className="grid grid-cols-1 pr-5 pl-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-4xl">
           <div className="max-h-1/4 grid gap-4">
             {data

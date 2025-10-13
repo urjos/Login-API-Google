ee7d98bfb3015cc54ced3baa8b968ffbf552f21d
Josué Torres <josuejesusts@gmail.com>
2025-10-13 12:18:39 -0500

    feat: Mejora de responsividad de Footer

--- packages/frontend/src/components/layouts/AppLayout/Footer/index.tsx
+++ packages/frontend/src/components/layouts/AppLayout/Footer/index.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow-md m-4">
+    <footer className="bg-white rounded-lg shadow-lg inset-shadow-xs m-4">
       <div className="flex w-full justify-between mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2025{" "}
@@ -14,7 +14,7 @@ export const Footer = () => {
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-col items-start text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 lg:flex-row lg:mt-0">
+        <ul className="flex flex-col items-start text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 md:flex-row lg:flex-row lg:mt-0">
           <li>
             <a href="#" className="hover:underline me-4 md:me-6">
               About

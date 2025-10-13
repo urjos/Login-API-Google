e280f406535c020687a7af831d3594944790cb13
Josué Torres <josuejesusts@gmail.com>
2025-10-13 12:05:13 -0500

    feat: Mejora de responsividad en el Footer

--- packages/frontend/src/components/layouts/AppLayout/Footer/index.tsx
+++ packages/frontend/src/components/layouts/AppLayout/Footer/index.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow-sm m-4">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-white rounded-lg shadow-md m-4">
+      <div className="flex w-full justify-between mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2025{" "}
           <a
             href="https://flowbite.com/"
             className="hover:underline"
@@ -14,7 +14,7 @@ export const Footer = () => {
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-col items-start text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 lg:flex-row lg:mt-0">
           <li>
             <a href="#" className="hover:underline me-4 md:me-6">
               About

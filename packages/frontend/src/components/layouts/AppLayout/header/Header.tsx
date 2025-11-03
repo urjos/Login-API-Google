4907b09e66e552e14ad4502dea40c100f5c870a4
Josué Torres <josuejesusts@gmail.com>
2025-11-03 12:51:59 -0500

    feat: mejora de header usando NavLink para estado de actividad de la pagina actual

--- packages/frontend/src/components/layouts/AppLayout/header/Header.tsx
+++ packages/frontend/src/components/layouts/AppLayout/header/Header.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useSessionStore } from "../../../../entities/store/SessionStore";
 
 export const Header = () => {
@@ -142,74 +142,94 @@ export const Header = () => {
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:gap-10 lg:mt-0">
               <li>
-                <Link
+                <NavLink
                   to="/"
-                  className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent 
-                  lg:text-primary-700 lg:p-0"
-                  aria-current="page"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-white" : "text-gray-400"} 
+                    block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700
+                    hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                    lg:hover:text-primary-700 lg:p-0
+                    lg:dark:hover:text-white dark:hover:bg-gray-700 
+                    dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`
+                  }
                 >
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to="/company"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                  hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
-                  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 
-                  lg:dark:hover:text-white dark:hover:bg-gray-700 
-                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-white" : "text-gray-400"} 
+                    block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700
+                    hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                    lg:hover:text-primary-700 lg:p-0
+                    lg:dark:hover:text-white dark:hover:bg-gray-700 
+                    dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`
+                  }
                 >
                   Company
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to="/marketplace"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                  hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
-                  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 
-                  lg:dark:hover:text-white dark:hover:bg-gray-700 
-                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-white" : "text-gray-400"} 
+                    block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700
+                    hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                    lg:hover:text-primary-700 lg:p-0
+                    lg:dark:hover:text-white dark:hover:bg-gray-700 
+                    dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`
+                  }
                 >
                   Marketplace
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to="/features"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                  hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
-                  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 
-                  lg:dark:hover:text-white dark:hover:bg-gray-700 
-                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-white" : "text-gray-400"} 
+                    block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700
+                    hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                    lg:hover:text-primary-700 lg:p-0
+                    lg:dark:hover:text-white dark:hover:bg-gray-700 
+                    dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`
+                  }
                 >
                   Features
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to="/team"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                  hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
-                  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 
-                  lg:dark:hover:text-white dark:hover:bg-gray-700 
-                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-white" : "text-gray-400"} 
+                    block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700
+                    hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                    lg:hover:text-primary-700 lg:p-0
+                    lg:dark:hover:text-white dark:hover:bg-gray-700 
+                    dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`
+                  }
                 >
                   Team
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to="/contact"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                  hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
-                  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 
-                  lg:dark:hover:text-white dark:hover:bg-gray-700 
-                  dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-white" : "text-gray-400"} 
+                    block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700
+                    hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
+                    lg:hover:text-primary-700 lg:p-0
+                    lg:dark:hover:text-white dark:hover:bg-gray-700 
+                    dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`
+                  }
                 >
                   Contact
-                </Link>
+                </NavLink>
               </li>
               <li className="block lg:hidden">
                 <Link
